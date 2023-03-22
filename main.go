1dfd1933cdbdd80fddc16755266e63abe7b974da
Taweechai Yuenyang <krumii.it@gmail.com>
2023-03-22 15:05:49 +0700

    update sql

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/abe27/luckyapp/configs"
 	"github.com/abe27/luckyapp/routes"
@@ -13,9 +11,11 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
+
+	// "gorm.io/driver/mysql"
+
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"gorm.io/gorm/schema"
 )
 
 func init() {
@@ -30,24 +30,26 @@ func init() {
 	// }
 
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	dsn := fmt.Sprintf("%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DBUSER"), os.Getenv("DBHOST"), os.Getenv("DBPORT"), os.Getenv("DBNAME"))
-	//   db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := fmt.Sprintf("%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DBUSER"), os.Getenv("DBHOST"), os.Getenv("DBPORT"), os.Getenv("DBNAME"))
+	// //   db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// fmt.Printf("DNS: %s\n", dns)
-	configs.Store, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
-		DisableAutomaticPing:                     true,
-		DisableForeignKeyConstraintWhenMigrating: false,
-		SkipDefaultTransaction:                   true,
-		NowFunc: func() time.Time {
-			return time.Now().Local()
-		},
-		NamingStrategy: schema.NamingStrategy{
-			TablePrefix:   "tbt_", // table name prefix, table for `User` would be `t_users`
-			SingularTable: false,  // use singular table name, table for `User` would be `user` with this option enabled
-			NoLowerCase:   false,  // skip the snake_casing of names
-			NameReplacer:  strings.NewReplacer("CID", "Cid"),
-		},
-	})
+	// // fmt.Printf("DNS: %s\n", dns)
+	// configs.Store, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+	// 	DisableAutomaticPing:                     true,
+	// 	DisableForeignKeyConstraintWhenMigrating: false,
+	// 	SkipDefaultTransaction:                   true,
+	// 	NowFunc: func() time.Time {
+	// 		return time.Now().Local()
+	// 	},
+	// 	NamingStrategy: schema.NamingStrategy{
+	// 		TablePrefix:   "tbt_", // table name prefix, table for `User` would be `t_users`
+	// 		SingularTable: false,  // use singular table name, table for `User` would be `user` with this option enabled
+	// 		NoLowerCase:   false,  // skip the snake_casing of names
+	// 		NameReplacer:  strings.NewReplacer("CID", "Cid"),
+	// 	},
+	// })
+
+	configs.Store, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database")
