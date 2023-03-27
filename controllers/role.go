e0490a45bf02b2636ae26760148609f2e2c115e9
Taweechai Yuenyang <krumii.it@gmail.com>
2023-03-27 16:54:41 +0700

    update new api

--- controllers/role.go
+++ controllers/role.go
@@ -44,16 +44,33 @@ func PostRole(c *fiber.Ctx) error {
 	role.Title = frm.Title
 	role.Description = frm.Description
 	role.IsActive = frm.IsActive
-	// if err := configs.Store.Create(&role).Error; err != nil {
-	// 	r.Message = err.Error()
-	// 	return c.Status(fiber.StatusInternalServerError).JSON(&r)
-	// }
+	if err := configs.Store.Create(&role).Error; err != nil {
+		r.Message = err.Error()
+		return c.Status(fiber.StatusInternalServerError).JSON(&r)
+	}
 
 	for _, v := range frm.FrmRoleDetail {
+		var RoleDetail models.RoleDetail
+		RoleDetail.RoleID = role.ID
+		RoleDetail.PermissionID = v.ID
 		switch v.Type {
 		case "create":
+			RoleDetail.Create = v.Status
 		case "read":
+			RoleDetail.Write = v.Status
 		case "write":
+			RoleDetail.Read = v.Status
+		}
+
+		if err := configs.Store.FirstOrCreate(&RoleDetail, &models.RoleDetail{RoleID: role.ID, PermissionID: v.ID}).Error; err != nil {
+			r.Message = err.Error()
+			return c.Status(fiber.StatusInternalServerError).JSON(&r)
+		}
+
+		// After Create Save Data
+		if err := configs.Store.Save(&RoleDetail).Error; err != nil {
+			r.Message = err.Error()
+			return c.Status(fiber.StatusInternalServerError).JSON(&r)
 		}
 	}
 
